5a7b58ecbf0b97f588660c5b45e16aa9a0d00221
220100 <220100kd@yse-c.net>
2023-08-31 10:15:48 +0900

    itemsのGETリクエスト追加

--- app/main.py
+++ app/main.py
@@ -8,3 +8,10 @@ app = FastAPI()
 async def root():
     # JSON形式に変換され、HTTPレスポンスの本文としてクライアントに返される
     return {"message": "Hello World"}
+
+# /items/{item_id}へのHTTP GETリクエストに対する処理を定義
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
