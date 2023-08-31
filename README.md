092b5d19c97909be86f49195c5fe32a9c9bfdb44
220100 <220100kd@yse-c.net>
2023-08-31 10:35:48 +0900

    POSTリクエスト

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Try-fastAPI
+## インストール
+pip3 install -r requirements.txt
+
+## 起動
+cd app
+uvicorn main:app --reload 
+
+## curlコマンド；HTTP GET リクエスト
+### エンドポイント：ルート（/）
+curl localhost:8000
+### エンドポイント：/items/123
+curl "localhost:8000/items/123"
+### エンドポイント：/items/123 クエリ指定
+curl "localhost:8000/items/123?q=hoge"
+
+## curlコマンド；HTTP POST リクエスト
+curl -X POST "http://localhost:8000/items" -H "Content-Type: application/json" -d "{\"name\":\"おなまえ\",\"price\":123}"
+{"item_name":"おなまえ","twice price":246.0}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # FastAPIインスタンス
 app = FastAPI()
+
+# データの扱い設定
+class Item(BaseModel):
+    name: str
+    price: float
 
 # ルート（"/"）へのHTTP GETリクエストに対する処理を定義
 @app.get("/")
@@ -15,3 +21,8 @@ def read_item(item_id: int, q: str = None):
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+# /items へのHTTP POSTリクエストに対する処理を定義
+@app.post("/items")
+def update_item(item: Item):
+    return {"item_name": item.name, "twice price": item.price * 2}
